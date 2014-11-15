34eb2d40f782a539648aca2677a0674fa8213add
Nadeem Malik <nadeem244@gmail.com>
2014-11-14 20:14:15 -0500

    gets location automatically

--- app/src/main/java/com/hci/androidclock/MainActivity.java
+++ app/src/main/java/com/hci/androidclock/MainActivity.java
@@ -1,24 +1,40 @@
 package com.hci.androidclock;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.content.SharedPreferences;
-import android.graphics.Color;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationClient;
+
 import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.view.Menu;
 import android.os.Handler;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
-
+import android.widget.Toast;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import java.util.Calendar;
-import java.util.TimeZone;
 import java.util.Timer;
 import java.util.TimerTask;
 
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements GooglePlayServicesClient.ConnectionCallbacks,
+        GooglePlayServicesClient.OnConnectionFailedListener  {
+    // GLOBAL CONSTANTS
+    private final static int
+            CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
     // This is just a dummy counter that counts upwards. We'll display
     // the current count value in a TextView. Note: I like to prefix my member variables
     // by the underscore character--this is just a convention, others use 'm'. For example,
@@ -45,23 +61,35 @@ public class MainActivity extends Activity {
     boolean displayTimeZone;
     int clockTextColor;
 
+    LocationClient mLocationClient;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mLocationClient = new LocationClient(this, this, this);
+
         setContentView(R.layout.activity_main);
         restorePreferences();
 
         updateClock();
-
-       //SharedPreferences settings = getSharedPreferences("pref", 0);
-       // boolean silent = settings.getBoolean("silentMode", false);
-       // setSilent(silent);
     }
 
+    /* called when the activity becomes visible */
     protected void onStart() {
         super.onStart();
+        mLocationClient.connect();
 
         restorePreferences();
+    }
+
+    /*
+ * Called when the Activity is no longer visible.
+ */
+    @Override
+    protected void onStop() {
+        // Disconnecting the client invalidates it.
+        mLocationClient.disconnect();
+        super.onStop();
     }
 
     public void restorePreferences() {
@@ -156,4 +184,151 @@ public class MainActivity extends Activity {
         }, 0, 1000);
     }
 
+
+
+    // Methods needed for Google Play Services (which is needed to get user location)
+    // Define a DialogFragment that displays the error dialog
+    public static class ErrorDialogFragment extends DialogFragment {
+        // Global field to contain the error dialog
+        private Dialog mDialog;
+        // Default constructor. Sets the dialog field to null
+        public ErrorDialogFragment() {
+            super();
+            mDialog = null;
+        }
+        // Set the dialog to display
+        public void setDialog(Dialog dialog) {
+            mDialog = dialog;
+        }
+        // Return a Dialog to the DialogFragment.
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            return mDialog;
+        }
+    }
+
+
+    /*
+     * Handle results returned to the FragmentActivity
+     * by Google Play services
+     */
+    @Override
+    protected void onActivityResult(
+            int requestCode, int resultCode, Intent data) {
+        // Decide what to do based on the original request code
+        switch (requestCode) {
+            case CONNECTION_FAILURE_RESOLUTION_REQUEST :
+            /*
+             * If the result code is Activity.RESULT_OK, try
+             * to connect again
+             */
+                switch (resultCode) {
+                    case Activity.RESULT_OK :
+                    /*
+                     * Try the request again
+                     */
+                        break;
+                }
+        }
+    }
+    private boolean servicesConnected() {
+        // Check that Google Play services is available
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        // If Google Play services is available
+        if (ConnectionResult.SUCCESS == resultCode) {
+            // In debug mode, log the status
+            Log.d("Location Updates",
+                    "Google Play services is available.");
+            // Continue
+            return true;
+            // Google Play services was not available for some reason.
+            // resultCode holds the error code.
+        } else {
+            // Get the error dialog from Google Play services
+            Dialog errorDialog = GooglePlayServicesUtil.getErrorDialog(
+                    resultCode,
+                    this,
+                    CONNECTION_FAILURE_RESOLUTION_REQUEST);
+
+            // If Google Play services can provide an error dialog
+            if (errorDialog != null) {
+                // Create a new DialogFragment for the error dialog
+                ErrorDialogFragment errorFragment =
+                        new ErrorDialogFragment();
+                // Set the dialog in the DialogFragment
+                errorFragment.setDialog(errorDialog);
+                // Show the error dialog in the DialogFragment
+                errorFragment.show(this.getFragmentManager(),
+                        "Location Updates");
+            }
+        }
+
+        return false;
+    }
+
+
+    // Location services callbacks
+    /*
+     * Called by Location Services when the request to connect the
+     * client finishes successfully. At this point, you can
+     * request the current location or start periodic updates
+     */
+    @Override
+    public void onConnected(Bundle dataBundle) {
+        // Display the connection status
+        //String locStr = "LOCATION: " + mLocationClient.getLastLocation().getLatitude() + ", " + mLocationClient.getLastLocation().getLongitude();
+        //Toast.makeText(this, locStr, Toast.LENGTH_SHORT).show();
+        String locStr = "CONNECTED";
+        System.out.println(locStr);
+    }
+
+    /*
+    * Called by Location Services if the connection to the
+    * location client drops because of an error.
+    */
+    @Override
+    public void onDisconnected() {
+        // Display the connection status
+        Toast.makeText(this, "Disconnected. Please re-connect.",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    /*
+     * Called by Location Services if the attempt to
+     * Location Services fails.
+     */
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        /*
+         * Google Play services can resolve some errors it detects.
+         * If the error has a resolution, try sending an Intent to
+         * start a Google Play services activity that can resolve
+         * error.
+         */
+        if (connectionResult.hasResolution()) {
+            try {
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(
+                        this,
+                        CONNECTION_FAILURE_RESOLUTION_REQUEST);
+                /*
+                 * Thrown if Google Play services canceled the original
+                 * PendingIntent
+                 */
+            } catch (IntentSender.SendIntentException e) {
+                // Log the error
+                e.printStackTrace();
+            }
+        } else {
+            /*
+             * If no resolution is available, display a dialog to the
+             * user with the error.
+             */
+            Toast.makeText(this, "Connection to Google Play services failed", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
+
+
+
